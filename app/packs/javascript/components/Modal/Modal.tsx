bca02190d70f4dfb36afbac563ff70596a122b2b
Miguel Prada <mapra99@gmail.com>
2021-08-08 21:41:47 -0500

    Close modal when ESC key pressed

--- app/packs/javascript/components/Modal/Modal.tsx
+++ app/packs/javascript/components/Modal/Modal.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CrossFilledIcon } from '../../icons/CrossFilledIcon';
 import {
   ModalContainer,
   ModalDialog,
   ModalCloseButton
 } from './Modal.styled';
+import { useKeyDown } from '../../hooks/useKeyDown';
 
 import { ModalProps } from './types';
 
 export const Modal = ({ children, onClose }: ModalProps) => {
+  const { keyCode } = useKeyDown();
+
+  useEffect(() => {
+    if (keyCode === 27) onClose() // Close modal when ESC key pressed
+  }, [keyCode])
+
   return (
     <ModalContainer onKeyDown={() => console.log("holi")}>
       <ModalDialog>
