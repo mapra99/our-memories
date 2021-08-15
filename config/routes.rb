eee70f96be1c5876fd79b0dc5b3594495cc36046
Miguel Prada <mprada.ing@gmail.com>
2021-08-15 15:39:11 -0500

    Devis init config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # todo uncomment this, when Ui is ready
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
   root to: 'react/posts#index'
 
   # react namespace
---
+++ db/migrate/20210815202252_delete_password_to_users.rb
@@ -0,0 +1,6 @@
+class DeletePasswordToUsers < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :users, :password
+    remove_column :users, :password_confirmation
+  end
+end
