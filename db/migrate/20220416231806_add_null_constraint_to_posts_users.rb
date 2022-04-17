a982cee07f14e2dd2320bb74da19e3d7a3e724d0
Miguel Prada <mapra99@gmail.com>
2022-04-16 19:29:52 -0500

    Reverted last migration

--- db/migrate/20220416231806_add_null_constraint_to_posts_users.rb
+++
@@ -1,5 +0,0 @@
-class AddNullConstraintToPostsUsers < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :posts, :user_id, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_16_231806) do
+ActiveRecord::Schema.define(version: 2022_04_16_225755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2022_04_16_231806) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["title"], name: "index_posts_on_title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
