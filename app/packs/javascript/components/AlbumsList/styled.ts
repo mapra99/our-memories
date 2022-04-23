916779913404dd63be84b12840c4ed95f7d54b0e
Miguel Prada <mapra99@gmail.com>
2022-04-23 18:58:36 -0500

    Fix layout on mobile

--- app/packs/javascript/components/AlbumsList/styled.ts
+++ app/packs/javascript/components/AlbumsList/styled.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { BREAKPOINT_L } from '../../constants';
 
 export const AlbumsListContainer = styled.div `
   display: grid;
   gap: 46px;
   margin: 23px 0;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
+
+  @media (min-width: ${BREAKPOINT_L}px) {
+    grid-template-columns: 1fr 1fr;
+  }
 `;
